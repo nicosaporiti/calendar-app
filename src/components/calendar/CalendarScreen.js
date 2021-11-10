c4594703936a06dfac6e96801835cf170fcef570
Nicolas Saporiti <nicolas.saporiti@gmail.com>
2021-11-09 22:57:52 -0300

    first commit

---
+++ src/components/calendar/CalendarScreen.js
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+import { NavBar } from "../ui/NavBar";
+import moment from "moment";
+import "moment/locale/es";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import { messages } from "../../helpers/calendar-messages-es";
+import CalendarEvent from "./CalendarEvent";
+import { CalendarModal } from "./CalendarModal";
+
+moment.locale("es");
+
+const localizer = momentLocalizer(moment);
+
+const events = [
+  {
+    title: "Cumpleaños del jefe - ",
+    start: moment().toDate(),
+    end: moment().toDate(),
+    bgcolor: "#fafafa",
+    notes: "Comprar el pastel",
+    user: {
+      _id: "123",
+      name: "Nicolas",
+    },
+  },
+];
+
+export const CalendarScreen = () => {
+  const [lastView, setLastView] = useState(
+    localStorage.getItem("lastView") || "month"
+  );
+
+  const onDoubleClick = (e) => {
+    console.log(e);
+  };
+
+  const onSelectEvent = (e) => {
+    console.log(e);
+  };
+
+  const onViewChange = (e) => {
+    setLastView(e);
+    localStorage.setItem("lastView", e);
+  };
+  const eventStyleGetter = (event, start, end, isSelected) => {
+    const style = {
+      backgroundColor: "#367CF7",
+      borderRadius: "0px",
+      opacity: 0.8,
+      display: "block",
+      color: "white",
+    };
+
+    return style;
+  };
+  return (
+    <div className="calendar-screen">
+      <NavBar />
+
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        messages={messages}
+        eventPropGetter={eventStyleGetter}
+        onDoubleClickEvent={onDoubleClick}
+        onSelectEvent={onSelectEvent}
+        onView={onViewChange}
+        view={lastView}
+        components={{
+          event: CalendarEvent,
+        }}
+      />
+
+      <CalendarModal />
+    </div>
+  );
+};
